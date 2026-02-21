45f35d3b468a27e56f499c5e1a1bf0234f5fe10b
yykzjh <1378453948@qq.com>
2026-02-21 19:58:10 +0000

    chore: use custom container image build from base

--- .devcontainer/scripts/modify_user_info.sh
+++ .devcontainer/scripts/modify_user_info.sh
@@ -4,8 +4,8 @@
 CURRENT_UID=$(id -u)
 CURRENT_GID=$(id -g)
 
-# update USER_UID and USER_GID values in devcontainer.json file
-DEVCONTAINER_JSON="./.devcontainer/devcontainer.json"
+# update USER_UID and USER_GID values in devcontainer_base.json file
+DEVCONTAINER_JSON="./.devcontainer/devcontainer_base.json"
 
 if [ -f "$DEVCONTAINER_JSON" ]; then
     echo "updating $DEVCONTAINER_JSON file..."
@@ -14,7 +14,7 @@ if [ -f "$DEVCONTAINER_JSON" ]; then
     sed -i "s/\"USER_UID\": \"\${localEnv:USER_UID}\"/\"USER_UID\": $CURRENT_UID/g" "$DEVCONTAINER_JSON"
     sed -i "s/\"USER_GID\": \"\${localEnv:USER_GID}\"/\"USER_GID\": $CURRENT_GID/g" "$DEVCONTAINER_JSON"
     
-    echo "successfully updated devcontainer.json file:"
+    echo "successfully updated devcontainer_base.json file:"
     echo "  USER_UID: $CURRENT_UID"
     echo "  USER_GID: $CURRENT_GID"
 else
